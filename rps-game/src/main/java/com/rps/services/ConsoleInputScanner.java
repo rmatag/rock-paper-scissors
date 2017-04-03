72584a509e2f6fd78d638068e54a92d342b45c32
Raul Mata <rmata@theworkshop.com>
2017-04-03 09:05:27 +0200

    Version 2 of game: Keep showing menu while not valid input and throw exception in case not numeric input

--- rps-game/src/main/java/com/rps/services/ConsoleInputScanner.java
+++ rps-game/src/main/java/com/rps/services/ConsoleInputScanner.java
@@ -1,19 +1,49 @@
 package com.rps.services;
 
+import java.io.InputStream;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ConsoleInputScanner {
+    private static final int FIRST_CORRECT_OPTION = 1;
+    private static final int LAST_CORRECT_OPTION = 4;
+
+    private InputStream in;
+
+    public ConsoleInputScanner() {
+        this.in = System.in;
+    }
+
+    public ConsoleInputScanner(InputStream in) {
+        this.in = in;
+    }
 
     public int getOptionFromUser() {
-        Scanner input = new Scanner(System.in);
+        boolean correctSelectedOption = false;
+        int option = -1;
 
-        System.out.println("\n\nChoose one Game Mode");
-        System.out.println("-------------------------\n");
-        System.out.println("1 - FAIR");
-        System.out.println("2 - UNFAIR");
-        System.out.println("3 - REMOTE");
-        System.out.println("4 - QUIT");
+        try {
 
-        return input.nextInt();
+            while(!correctSelectedOption) {
+                Scanner input = new Scanner(this.in);
+
+                System.out.println("\n\nChoose one Game Mode");
+                System.out.println("-------------------------\n");
+                System.out.println("1 - FAIR");
+                System.out.println("2 - UNFAIR");
+                System.out.println("3 - REMOTE");
+                System.out.println("4 - QUIT");
+
+
+                option = input.nextInt();
+                correctSelectedOption = option >= FIRST_CORRECT_OPTION && option <= LAST_CORRECT_OPTION;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Please, choose a number as option :)");
+            throw e;
+        }
+
+        return option;
+
     }
 }
